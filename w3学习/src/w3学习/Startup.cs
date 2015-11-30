8b421cdf91eb924902b27422f85db61855ae23f4
Linda <liguilin0522@qq.com>
2015-11-30 18:16:45 +0800

    将ASP.NET vNext版本从beta5升级为rc1。

--- w3学习/src/w3学习/Startup.cs
+++ w3学习/src/w3学习/Startup.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace w3学习
 {
     public class Startup
     {
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection env)
         {
         }
 
@@ -20,5 +21,7 @@ namespace w3学习
             app.UseStaticFiles();
             app.UseDirectoryBrowser();
         }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
